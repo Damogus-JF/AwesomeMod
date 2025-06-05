e51be72e2068fb37033c38dee3394040034c78ec
Damogus-JF <jacobfrazier8811@gmail.com>
2025-06-05 15:54:28 -0500

    Added Chud joker

--- slop/jokers.lua
+++ slop/jokers.lua
@@ -10,8 +10,8 @@ SMODS.Joker {
     loc_txt = {
         name = 'White Monster',
         text = {
-            'Gives {X:mult,C:white} X#1# {} mult {}',
-            'Decreases by {X:mult,C:white} X#2# {} mult {} at the end of round'
+            '{X:mult,C:white}X#1#{} mult {}',
+            'Decreases by {X:mult,C:white}X#2#{} mult {} at the end of round'
         }
     },
     atlas = 'AwesomeAtlas', pos = { x = 0, y = 0},
@@ -36,12 +36,10 @@ SMODS.Joker {
         if context.end_of_round and context.game_over == false and not context.repetition and not context.blueprint then
             card.ability.extra.Xmult = card.ability.extra.Xmult - card.ability.extra.NegXmult
             card:juice_up(0.8, 0.8)
-            if not card.ability.extra.Xmult <= 1 then
-                return {
-                    message = 'Sluuurp!',
-                    colour = G.C.MULT
-                }
-            end
+            return {
+                message = 'Sluuurp!',
+                colour = G.C.MULT
+            }
         end
         if card.ability.extra.Xmult <= 1 then
             SMODS.destroy_cards(card)
@@ -50,6 +48,59 @@ SMODS.Joker {
                 colour = G.C.CHIPS
             }
             
+        end
+    end
+}
+
+SMODS.Joker {
+    key = 'Chud',
+    loc_txt = {
+        name = 'Chud',
+        text = {
+            'Destroy the joker to the right of this',
+            'at the end of round and create',
+            'a {C:spectral}spectral{} card'
+        }
+    },
+    atlas = 'AwesomeAtlas', pos = { x = 2, y = 0},
+    config = {},
+    loc_vars = function (self, info_queue, card)
+        return {
+            vars = { }
+        } 
+    end,
+    rarity = 3,
+    cost = 8,
+    calculate = function (self, card, context)
+        if context.end_of_round and context.game_over == false and not context.repetition then
+            local mypos = nil
+            for i = 1, #G.jokers.cards do 
+                if G.jokers.cards[i] == card then mypos = i; break end
+            end
+            if mypos and G.jokers.cards[mypos+1] and not self.getting_sliced and not G.jokers.cards[mypos+1].ability.eternal and not G.jokers.cards[mypos+1].getting_sliced then 
+                local sliced_card = G.jokers.cards[mypos+1]
+                SMODS.destroy_cards(G.jokers.cards[mypos + 1])
+                G.E_MANAGER:add_event(Event({
+                    func = function ()
+                        card:juice_up(0.8,0.8)
+                        if #G.consumeables.cards + 1 <= G.consumeables.config.card_limit then
+                            local card = create_card('Spectral',G.consumeables, nil, nil, nil, nil, nil, 'sixth')
+                            card:add_to_deck()
+                            G.consumeables:emplace(card)
+                        end
+                        return true
+                    end
+                }))
+                return {
+                    message = 'Billions must die!',
+                    colour = G.C.MULT
+                }
+            else
+                return {
+                    message = 'Nothing ever happens...',
+                    colour = G.C.CHIPS
+                }
+            end
         end
     end
 }
